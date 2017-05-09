6eed44f5cb4be48c4093e717d09e9f9f31293b8c
sathi <psathibabu@gmail.com>
2017-05-09 19:00:53 +0530

    remove console.logs

--- tests/filetests.js
+++ tests/filetests.js
@@ -2,16 +2,12 @@
 
 var expect = require('chai').expect;
 
-
-
-
 describe('flatentest',function(){
     it('sample to check test inegrity for flatten',function(){
             var flatten = require('../lib/flatten.js');
             expect(flatten.flatten()).to.equal('flatten data super awesome');
     });
 });
-
 
 describe('filetest',function(){
     it('sample to check test inegrity for filetest',function(){
